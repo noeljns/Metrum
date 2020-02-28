e38fec070a7e35016d73adf6648204371de16569
Jonas <jonas@MacBook-Pro.fritz.box>
2020-02-28 21:29:31 +0100

    force portrait orientation

--- Metrum/GameViewController.swift
+++ Metrum/GameViewController.swift
@@ -31,10 +31,11 @@ class GameViewController: UIViewController {
                 view.presentScene(scene)
             }
             
-            view.ignoresSiblingOrder = false
+            // https://www.hackingwithswift.com/articles/184/tips-to-optimize-your-spritekit-game
+            view.ignoresSiblingOrder = true
 
-            view.showsFPS = true
-            view.showsNodeCount = true
+            view.showsFPS = false
+            view.showsNodeCount = false
         }
     }
 
